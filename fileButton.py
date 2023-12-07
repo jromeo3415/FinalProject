c0e9d6eb20b25b49b7763a8e7e71e99b812d7052
jromeo3415 <151575928+jromeo3415@users.noreply.github.com>
2023-12-07 14:22:41 -0500

    Button with full functionality

--- fileButton.py
+++ fileButton.py
@@ -1,7 +1,21 @@
 import tkinter as tk
-import fetchFile
+from tkinter.filedialog import askopenfile
+import os
+
+def fetchFile():    #defining button command to open file explorer and then display selected file
+    file = askopenfile(mode = 'r')
+    if file:
+        file_path = file.name
+        file_name = os.path.basename(file_path)
+        fileDisplay.insert(tk.END, file_name)
+        file.close()
 
 root = tk.Tk()
-root.geometry(100, 100)
+root.geometry('455x50')
 root.title('Sound Modeler')
-button = tk.button(root, text='Select File', width = 15, command = fetchFile)
+button = tk.Button(root, text='Select File', width = 40, command = fetchFile)
+button.grid(row=0, column = 0)
+
+fileDisplay = tk.Listbox(root, height=2, width= 75)
+fileDisplay.grid(row=1, column = 0)
+root.mainloop()
