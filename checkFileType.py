c1673000db1fa9944f9ff0b1fbb92e6a5920c68b
jromeo3415 <151575928+jromeo3415@users.noreply.github.com>
2023-12-07 19:18:07 -0500

    need to make it change the actual file
    Current version may be good enough. Should change the actual file instead of returning an object or a file.

--- checkFileType.py
+++ checkFileType.py
@@ -2,12 +2,20 @@ import os.path
 from pydub import AudioSegment
 
 
-def checkType(file):
+def checkType(file):    #needs to be changed so the file is changed at the location it is at instead of returning an object (already did?)
     fileExtension = os.path.splitext(file)
     if fileExtension == ".wav":
         return file
     elif fileExtension == ".mp3":
         temp = AudioSegment.from_mp3(file)
-        temp.export(temp, format='wav')
+        temp.export(file.path, format='wav')
         print(temp)
         return temp
+    elif fileExtension == ".aac":
+        temp = AudioSegment.from_mp3(file)
+        temp.export(file.path, format='wav')
+        print(temp)
+        return temp
+    else:
+        print("Incompatible file type")
+        return
