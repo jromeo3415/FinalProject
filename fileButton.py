e3a8690a3bae99c75f34b085727eeb916a516727
jromeo3415 <151575928+jromeo3415@users.noreply.github.com>
2023-12-10 13:59:12 -0500

    merge button almost working
    function created to merge the graphs but need to somehow make file_path global

--- fileButton.py
+++ fileButton.py
@@ -2,12 +2,14 @@ import tkinter as tk
 from tkinter.filedialog import askopenfile
 import os
 from FinalProject.wavTools import checkType, checkStereo, checkMetaData, makeWavArray
-from FinalProject.graphingTools import plotAll, plotSpectogram
-from teset import plotrt60
+from FinalProject.graphingTools import plotWaveform, plotSpectogram, plotrt60, combinedPlots
+
+
 
 def fetchFile():    #defining button command to open file explorer and then display selected file
     file = askopenfile(mode = 'r')
     if file:
+        global file_path
         file_path = file.name
         file_name = os.path.basename(file_path)
         fileDisplay.insert(tk.END, file_name)
@@ -20,30 +22,38 @@ def fetchFile():    #defining button command to open file explorer and then disp
         fileDisplay.insert(tk.END, f"File length: {lengthSec} seconds") #displaying length in listbox
 
         wavArray = makeWavArray(file_path) #making array of the file's data
-        plotAll(length, wavArray)
+        plotWaveform(length, wavArray)
 
-        spectogram = plotSpectogram(file_path)
+        plotSpectogram(file_path)
 
+        rt1, max1 = plotrt60(file_path, 250, "Low Frequency")
+        rt2, max2 = plotrt60(file_path, 1000, "Mid Frequency")
+        rt3, max3 = plotrt60(file_path, 10000, "High Frequency")
 
-        low = (20, 200)
-        mid = (20, 2000)
-        high = (2000, 20000)
+        fileDisplay.insert(tk.END, f"Low Frequency RT60: {rt1}")
+        fileDisplay.insert(tk.END, f"Mid Frequency RT60: {rt2}")
+        fileDisplay.insert(tk.END, f"High Frequency RT60: {rt3}")
 
-        plotrt60(file_path, 250, "Low Frequency")
-        plotrt60(file_path, 1000, "Mid Frequency")
-        plotrt60(file_path, 10000, "High Frequency")
-
+        combinedPlots(file_path, colors)
         file.close()
 
 
-#GUI implementation
+#GUI
 root = tk.Tk()
 root.geometry('455x120')
 root.title('Sound Modeler')
-button = tk.Button(root, text='Select File', width = 40, command = fetchFile)
+button = tk.Button(root, text='Select File', width = 25, command = fetchFile)
 button.grid(row=0, column = 0)
 
+colors = ['#004bc6', '#c6000a', '#6a00c6']
+mergeButton = tk.Button(root, text='Merge Graphs', width=25)
+mergeButton.grid(row=0, column=1)
+
 fileDisplay = tk.Listbox(root, height=5, width= 75)
-fileDisplay.grid(row=1, column = 0)
+fileDisplay.grid(row=1, column = 0, columnspan=2)
+
+scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL, command=fileDisplay.yview)
+scrollbar.grid(row=1, column=2, sticky=tk.N + tk.S)
+fileDisplay.config(yscrollcommand=scrollbar.set)
 
 root.mainloop()
--- graphingTools.py
+++ graphingTools.py
@@ -1,39 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import librosa
 from scipy.io import wavfile
 
-def plotAll(length, wavArray):
+def plotWaveform(length, wavArray):
     time = np.linspace(0., length, wavArray.shape[0])
     plt.plot(time, wavArray)
     plt.title('Waveform')
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude")
     plt.show()
-
-def calculatert60(filePath, lowFreq, midFreq, highFreq):
-    y, sr = librosa.load(filePath, sr=None)
-
-    lowBand = librosa.filters.mel(sr=sr, n_fft=2048, fmin=lowFreq[0], fmax=lowFreq[1])
-    midBand = librosa.filters.mel(sr=sr, n_fft=2048, fmin=midFreq[0], fmax=midFreq[1])
-    highBand = librosa.filters.mel(sr=sr, n_fft=2048, fmin=highFreq[0], fmax=highFreq[1])
-
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
-
-    lowSpec = np.dot(lowBand, D)
-    midSpec = np.dot(midBand, D)
-    highSpec = np.dot(highBand, D)
-
-    rt60_low = librosa.effects.estimate_rt60(lowSpec, sr=sr)
-    rt60_mid = librosa.effects.estimate_rt60(midSpec, sr=sr)
-    rt60_high = librosa.effects.estimate_rt60(highSpec, sr=sr)
-
-'''def plotrt60(rt60, freqBand):
-    plt.bar(0, rt60)
-    plt.title(f'RT60 for ')
-    plt.xlabel('Frequency Bands')
-    plt.ylabel('RT60 (seconds)')
-    plt.show()'''
 
 def plotSpectogram(filePath):
     sample_rate, data = wavfile.read(filePath)
@@ -46,16 +21,98 @@ def plotSpectogram(filePath):
     plt.show()
     return spectrum
 
-def find_target_frequency(freqs):
+def find_target_frequency(freq_band):
     for x in freqs:
-        if x > 1000:
+        if x > freq_band:
             break
     return x
 
-def frequency_check(freqs, spectrum):
-    target_frequency = find_target_frequency(freqs)
+def frequency_check(freq_band):
+    target_frequency = find_target_frequency(freq_band)
     index_of_frequency = np.where(freqs == target_frequency)[0][0]
     data_for_frequency = spectrum[index_of_frequency]
     data_in_db_fun = 10 * np.log10(data_for_frequency)
     return data_in_db_fun
 
+def find_nearest_value(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def plotrt60(file_path, freq_band, freq_name):
+    sample_rate, data = wavfile.read(file_path)
+    global spectrum, freqs, t, im
+    spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+    plt.close()
+
+    data_in_db = frequency_check(freq_band)
+    plt.figure()
+    plt.title(freq_name)
+    plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color ='#004bc6')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Power (dB)')
+
+    index_of_max = np.argmax(data_in_db)
+    value_of_max = data_in_db[index_of_max]
+    plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+
+    sliced_array = data_in_db[index_of_max:]
+
+    value_of_max_less_5 = value_of_max - 5
+
+    value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+    index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+    plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+    value_of_max_less_25 = value_of_max - 25
+    value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+    index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+
+    plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+    rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])[0]
+
+    rt60 = rt20 * 3
+
+    plt.grid()
+    plt.show()
+
+    return rt60, value_of_max
+
+def combinedPlots(file_path, colors):
+    sample_rate, data = wavfile.read(file_path)
+
+    spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+    plt.close()
+
+    plt.figure()
+    plt.title("Combined RT60's")
+    plt.xlabel('Time (s)')
+    plt.ylabel('Power (dB)')
+
+    freq_bands = [250, 1000, 10000]
+    for x in range(3):
+        data_in_db = frequency_check(freq_bands[x])
+        plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color=colors[x])
+        index_of_max = np.argmax(data_in_db)
+        value_of_max = data_in_db[index_of_max]
+        plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+
+        sliced_array = data_in_db[index_of_max:]
+
+        value_of_max_less_5 = value_of_max - 5
+
+        value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+        index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+        plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+        value_of_max_less_25 = value_of_max - 25
+        value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+        index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+
+        plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+        #rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])[0]
+
+        #rt60 = rt20 * 3
+
+    plt.grid()
+    plt.show()
